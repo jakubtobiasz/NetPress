605825f8831ec4be8eb3114f47135e6b1c036a14
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2021-03-02 22:21:44 +0100

    Initial commit

---
+++ NetPress/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetPress.Data;
+using NetPress.Models;
+
+[assembly: HostingStartup(typeof(NetPress.Areas.Identity.IdentityHostingStartup))]
+namespace NetPress.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
